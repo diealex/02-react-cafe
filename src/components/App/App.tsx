28130cfc8d76b2028765b89173a29bf8fa831989
Alex <diealex@users.noreply.github.com>
2025-05-14 17:38:11 +0200

    normalize + component folders added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import css from './App.module.css'
-import CafeInfo  from './CafeInfo.tsx'
-import VoteOptions from './VoteOptions.tsx'
-import VoteStats from './VoteStats.tsx'
-import Notification from './Notification.tsx'
+import normalize from './App.css'
+import CafeInfo  from '../CafeInfo/CafeInfo.tsx'
+import VoteOptions from '../VoteOptions/VoteOptions.tsx'
+import VoteStats from '../VoteStats/VoteStats.tsx'
+import Notification from '../Notification/Notification.tsx'
 import { useState } from 'react';
-import type { VoteType } from '../types/votes.ts';
+import type { VoteType } from '../../types/votes.ts';
 
 export default function App() {
 
